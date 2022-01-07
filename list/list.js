006f762e4dd16ef89f3837d0b91592ec41bc3dd2
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-01-06 17:49:27 -0800

    form event listener written

--- list/list.js
+++ list/list.js
@@ -14,7 +14,45 @@ const deleteButtonEl = document.querySelector('#delete-button');
 const listEl = document.querySelector('#list-items');
 const logoutButtonEl = document.querySelector('#logout');
 
-console.log(formEl, deleteButtonEl, listEl, logoutButtonEl);
+window.addEventListener('load', async() => {
+    //fetch and display user's existing list items
+    // call supabase to fetch all shopping items for this user
+    await getItems();
+
+
+    displayShoppingListItems();
+});
+
+formEl.addEventListener('submit', async(e) => {
+    e.preventDefault();
+
+    const data = new FormData('list');
+
+    const listItem = data.get('item');
+    const itemQuantity = data.get('quantity');
+
+    await createItem(listItem, itemQuantity);
+
+    formEl.reset();
+
+    displayShoppingListItems();
+})
+
+async function displayShoppingListItems() {
+    // fetches the items, clears out the list, and redisplays them
+    //loop through those items, create DOM elements, and append -- render items differently if "bought: true"
+
+    await getItems();
+
+    listEl.textContent = '';
+
+    for (let item of items) {
+        const 
+    }
+
+
+}
+
 
 logoutButtonEl.addEventListener('click', () => {
     logout();
